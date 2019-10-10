fba3f3408ff16d38d5d0f220828cb8ac9e8c603c
Dexhigh <dexhigh@Dexhighs-Mac-mini.local>
2019-10-10 11:23:57 +0530

    kakao

--- src/App.js
+++ src/App.js
@@ -1,75 +1,128 @@
-import React, { Fragment} from 'react';
+import React, { Fragment, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 import KakaoLogin from 'react-kakao-login';
 import ReactDOM from 'react-dom';
 import styled from 'styled-components';
+import {useCookies} from 'react-cookie';
+import Cookies from 'universal-cookie';
 
+const cookies = new Cookies();
+let d = new Date();
+d.setTime(d.getTime() + (1*60*0.1));
+cookies.set('myCat', 'Pacman', { path: '/', expires: d });
+console.log(cookies.get('myCat')); // Pacman
+
+
+
+
+
+
+// componentDidMount() {
+//   axios.get(`https://jsonplaceholder.typicode.com/users`)
+//     .then(res => {
+//       const persons = res.data;
+//       this.setState({ persons });
+//     })
+// }
+
+
+// const setCookie=(minutes) => {
+//   let d = new Date();
+//   d.setTime(d.getTime() + (minutes*60*0.1));
+
+//   useCookies.set("onboarded", true, {path: "/", expires: d});
+// };
 
 function App() {
 
+useEffect(
+() => {
+    axios.get(`https://kauth.kakao.com/oauth/authorize?client_id=bb2ee98827cc60db7edf12117ad26fca&redirect_uri=www.google.com&response_type=code`, {headers: {
+      'Access-Control-Allow-Headers': 'Authorization, KA, Origin, X-Requested-With, Content-Type, Accept',
+      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+      'Access-Control-Allow-Origin': '*'
+    }})
+      .then(res => {
+        // const persons = res.data;
+        // this.setState({ persons });
+        console.log("conosle", res);
+      })
+      .catch(error => {
+        console.log(error)
+        
+    })
+  
+    }
+)
+  
   return (
-    <div className="App">
+    <div className="App" >
       <header className="App-header">
 
+
       <Fragment>
-                <p><code>No options</code></p>
-                <KakaoLogin
-                  jsKey={key}
-                  onSuccess={success}
-                  onFailure={failure}
-                />
-                <p>Change button text with <code>buttonText</code></p>
-                <KakaoLogin
-                  jsKey={key}
-                  onSuccess={success}
-                  onFailure={failure}
-                  buttonText="Button Text"
-                />
-                <p>Use style that is defined in KakaoLogin component with <code>useDefaultStyle</code></p>
-                <KakaoLogin
-                  jsKey={key}
-                  onSuccess={success}
-                  onFailure={failure}
-                  useDefaultStyle
-                />
-                <p>Pass component that is styled as <code>children</code></p>
-                <KakaoLogin
-                  jsKey={key}
-                  onSuccess={success}
-                  onFailure={failure}
+            {/* <p><code>No options</code></p>
+            <KakaoLogin
+              jsKey={key}
+              onSuccess={success}
+              onFailure={failure}
+            />
+            <p>Change button text with <code>buttonText</code></p>
+            <KakaoLogin
+              jsKey={key}
+              onSuccess={success}
+              onFailure={failure}
+              buttonText="Button Text"
+            /> */}
+            <p> KakaoLogin component with <code>useDefaultStyle</code></p>
+            <KakaoLogin
+              jsKey={key}
+              onSuccess={success}
+              onFailure={failure}
+              useDefaultStyle
+            />
+
+
+            {/* <p>Pass component that is styled as <code>children</code></p>
+            <KakaoLogin
+              jsKey={key}
+              onSuccess={success}
+              onFailure={failure}
+            >
+              <Italic>Children</Italic>
+            </KakaoLogin>
+            <p>Pass <code>className</code> to style component</p>
+            <KakaoLogin
+              jsKey={key}
+              onSuccess={success}
+              onFailure={failure}
+              className="css-with-class"
+            />
+            <p>Pass <code>render</code> function to render fully customized component</p>
+            <KakaoLogin
+              jsKey={key}
+              onSuccess={success}
+              onFailure={failure}
+              render={props => (
+                <a
+                  href="#"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    props.onClick();
+                  }}
                 >
-                  <Italic>Children</Italic>
-                </KakaoLogin>
-                <p>Pass <code>className</code> to style component</p>
-                <KakaoLogin
-                  jsKey={key}
-                  onSuccess={success}
-                  onFailure={failure}
-                  className="css-with-class"
-                />
-                <p>Pass <code>render</code> function to render fully customized component</p>
-                <KakaoLogin
-                  jsKey={key}
-                  onSuccess={success}
-                  onFailure={failure}
-                  render={props => (
-                    <a
-                      href="#"
-                      onClick={(e) => {
-                        e.preventDefault();
-                        props.onClick();
-                      }}
-                    >
-                      Render Prop
-                    </a>
-                  )}
-                />
-                <p>Use <code>third party</code>, like <code>styled-components</code></p>
-                <StyledKakaoLogin
-                  jsKey={key}
-                  onSuccess={success}
-                  onFailure={failure}
-                />
+                  Render Prop
+                </a>
+              )}
+            />
+            <p>Use <code>third party</code>, like <code>styled-components</code></p>
+            <StyledKakaoLogin
+              jsKey={key}
+              onSuccess={success}
+              onFailure={failure}
+            /> */}
+            
         </Fragment>
          
       </header>
@@ -78,10 +131,11 @@ function App() {
 }
 
 
-const key = '4a5607f2dc1622d91b7137fff35a464d';
+const key = 'e8f211096da02fceca5f216fbf78b0b4';
 
 const success = (response) => {
   console.log(response);
+  //setCookie(1);
 };
 
 const failure = (error) => {
